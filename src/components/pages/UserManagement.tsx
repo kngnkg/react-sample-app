58126af7835f26a2e15e4fddad4dbfac8dbc5e42
ryokawa <kawata.ryo.0518@gmail.com>
2022-12-31 13:05:35 +0900

    add usercard

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,10 +1,39 @@
-import { FC, memo } from "react"
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react"
+import { useAllUsers } from "../../hooks/useAllUsers";
+
+import { UserCard } from "../organisms/user/UserCard";
 import { HeaderLayout } from "./templates/HeaderLayout";
 
 export const UserManagement: FC = memo(() => {
+    const { getUsers, users, loading } = useAllUsers();
+
+    // 初回のみ
+    useEffect(() => getUsers(), [])
+
     return (
+        <>
         <HeaderLayout>
-            <p>ユーザー管理ページ</p>
-        </HeaderLayout>
+            {loading ? (
+                <Center h="100vh">
+                    <Spinner />
+                </Center>
+            ) : (
+                <Wrap p={{ base: 4, md: 10 }}>
+                    {users.map((user) => (
+                        // TODO: mx="auto"が効かない
+                        <WrapItem key={user.id} mx="auto">
+                            <UserCard
+                                imageUrl="https://source.unsplash.com/random"
+                                userName={user.username}
+                                fullName={user.name}
+                            />
+                        </WrapItem>
+                    ))}
+                    
+                </Wrap>
+            )}
+            </HeaderLayout>
+        </>
     );
 });
