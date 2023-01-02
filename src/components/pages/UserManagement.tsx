5ffdd210e5df5700276bb5d3543e4b7015c3c4f7
ryokawa <kawata.ryo.0518@gmail.com>
2023-01-02 10:25:49 +0900

    add userdetailmodal

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,15 +1,24 @@
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
-import { FC, memo, useEffect } from "react"
+import { Center,Spinner, useDisclosure, Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useCallback, useEffect } from "react"
 import { useAllUsers } from "../../hooks/useAllUsers";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 import { HeaderLayout } from "./templates/HeaderLayout";
 
 export const UserManagement: FC = memo(() => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const { getUsers, users, loading } = useAllUsers();
+    const { onSelectUser, selectedUser } = useSelectUser();
 
     // 初回のみ
-    useEffect(() => getUsers(), [])
+    useEffect(() => getUsers(), [getUsers]);
+
+    //propsで渡す関数は毎回再作成するとレンダリング効率が悪い
+    const onClickUser = useCallback((id: number) => {
+        onSelectUser({ id, users, onOpen });
+    },[onOpen, onSelectUser, users]);
 
     return (
         <>
@@ -24,15 +33,18 @@ export const UserManagement: FC = memo(() => {
                         // TODO: mx="auto"が効かない
                         <WrapItem key={user.id} mx="auto">
                             <UserCard
+                                id={user.id}
                                 imageUrl="https://source.unsplash.com/random"
                                 userName={user.username}
                                 fullName={user.name}
+                                onClick={onClickUser}
                             />
                         </WrapItem>
                     ))}
                     
                 </Wrap>
             )}
+            <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
             </HeaderLayout>
         </>
     );
